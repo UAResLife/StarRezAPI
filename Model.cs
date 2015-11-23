c1f4dc0fe458cdd889e9b8e111a5c16f0716885b
Chad Nierenhausen <cnieren@email.arizona.edu>
2015-11-23 16:26:22 -0700

    Generalized Template

---
+++ Model.cs
@@ -0,0 +1,12 @@
+﻿
+using Newtonsoft.Json;
+
+namespace StarRezAPI
+{
+    [JsonObject]
+    public class Model
+    {
+        [JsonProperty("StudentID")]
+        public string StudentId { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -38,8 +38,7 @@ namespace StarRezAPI
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", auth);
 
                 // Async requests for Application and Booking data
-                GetApplications(client, queryString).Wait();
-                GetBookings(client, queryString).Wait();
+                GetResult(client, queryString).Wait();
             }
 
             // Pause to see results
@@ -47,18 +46,18 @@ namespace StarRezAPI
         }
 
         /**
-         * GetApplications
+         * GetResult
          *
-         * Make an async request to the appropriate endpoint to retreive Application data
+         * Make an async request to the appropriate endpoint
          *
          * @param client - An instance of an HttpClient, used to make the HTTP request
          * @param queryString - Query String to append to this request
          *
          */
-        static async Task GetApplications(HttpClient client, string queryString)
+        static async Task GetResult(HttpClient client, string queryString)
         {
             // The report and format for this request
-            const string report = "NextSteps_Applications.json"; 
+            const string report = ""; 
 
             // Make the request
             var response = await client.GetAsync(report +queryString);
@@ -68,62 +67,10 @@ namespace StarRezAPI
             {
                 // Pull the result from the response and convert to typed objects
                 var json = await response.Content.ReadAsStringAsync();
-                var apps = JsonConvert.DeserializeObject<List<Application>>(json);
+                // Change Model to solution specific object
+                var result = JsonConvert.DeserializeObject<Model>(json);
 
-                Console.WriteLine($"Applications for {apps[0].StudentId}");
-                foreach (var app in apps)
-                {
-                    Console.WriteLine($"\tTerm:\t\t\t\t {app.Term}");
-                    Console.WriteLine($"\tTerm Description:\t\t {app.TermDescription}");
-                    Console.WriteLine($"\tRating:\t\t\t\t {app.Rating}");
-                    Console.WriteLine($"\tStatus:\t\t\t\t {app.Status}");
-                    Console.WriteLine($"\tApp Fee Status:\t\t\t {app.FeeStatus}");
-                    Console.WriteLine($"\tCRP Status:\t\t\t {app.CrpStatus}");
-                    Console.WriteLine($"\tLicense Agreement Status:\t {app.LicenseAgreementStatus}");
-                    Console.WriteLine();
-                }
-            }
-            else
-            {
-                // Error code returned
-                Console.WriteLine("No records found.");
-            }
-        }
-
-        /**
-         * GetBookings
-         *
-         * Make an async request to the appropriate endpoint to retreive Booking data
-         *
-         * @param client - An instance of an HttpClient, used to make the HTTP request
-         * @param queryString - Query String to append to this request
-         *
-         */
-        static async Task GetBookings(HttpClient client, string queryString)
-        {
-            // The report and format for this request
-            const string report = "NextSteps_Bookings.json"; 
-
-            // Make the request
-            var response = await client.GetAsync(report + queryString);
-
-            // Check for a successfull result
-            if (response.IsSuccessStatusCode)
-            {
-                // Pull the result from the response and convert to typed objects
-                var json = await response.Content.ReadAsStringAsync();
-                var bookings = JsonConvert.DeserializeObject<List<Booking>>(json);
-
-                Console.WriteLine($"Bookings for {bookings[0].StudentId}");
-                foreach (var booking in bookings)
-                {
-                    Console.WriteLine($"\tTerm:\t\t\t {booking.Term}");
-                    Console.WriteLine($"\tTerm Description:\t {booking.TermDescription}");
-                    Console.WriteLine($"\tStatus:\t\t\t {booking.Status}");
-                    Console.WriteLine($"\tHall:\t\t\t {booking.HallName}");
-                    Console.WriteLine($"\tRoom:\t\t\t {booking.RoomType}");
-                    Console.WriteLine();
-                }
+                // Perform work on deserialized objects
             }
             else
             {
